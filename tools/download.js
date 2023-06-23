52d946d3b90cf7a2d4cb034cf4ff89238e66eeb7
Siddhant Vats <sidvats05@gmail.com>
2023-06-23 18:23:27 +0530

    feat: download from link

---
+++ tools/download.js
@@ -0,0 +1,27 @@
+//https://blobbydeol.blob.core.windows.net/kmicro/sidvats05/context.pdf
+const fs = require('fs')
+const https = require('https')
+
+const username = 'sidvats05';
+
+// File URL
+const url = `https://blobbydeol.blob.core.windows.net/kmicro/${username}/context.pdf`
+
+// Download the file
+https
+  .get(url, res => {
+    // Open file in local filesystem
+    const file = fs.createWriteStream(`./files/context.pdf`)
+
+    // Write data into local file
+    res.pipe(file)
+
+    // Close the file
+    file.on('finish', () => {
+      file.close()
+      console.log(`File downloaded!`)
+    })
+  })
+  .on('error', err => {
+    console.log('Error: ', err.message)
+  })
